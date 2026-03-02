bdc8cfdc177ef271dfae9a6780876d9180491717
sengee <sengeedorj@gmail.com>
2026-03-02 19:35:11 +0900

    updated frontend claude

--- fileconverter/urls.py
+++ fileconverter/urls.py
@@ -4,5 +4,5 @@ from . import views
 urlpatterns = [
     path('upload/', views.upload_file, name='upload_file'),
     path('name_request/', views.name_request, name='name_request'),
-    path('', views.home, name='home'),
+    path('', views.upload_file, name='home'),
 ]
